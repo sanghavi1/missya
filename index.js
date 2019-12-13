d22bdcf3641cc09f62f733c6929646d6058ed559
Yash Sanghavi <ysanghavi1@gmail.com>
2019-12-12 22:27:52 -0800

    yeh

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ var request = sg.emptyRequest({
 
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 5000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on ${PORT}/`);
 });
