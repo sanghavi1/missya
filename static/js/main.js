d9d3e969636e2f65a78e788b74b3e23ea9680135
Yash Sanghavi <ysanghavi1@gmail.com>
2019-12-13 20:15:00 -0800

    yeh

--- static/js/main.js
+++ static/js/main.js
@@ -3,11 +3,10 @@ var helper = require('sendgrid').mail;
 
 
 function sendEmail(sendTo, message){
-  console.log(sendTo, message);
   var from_email = new helper.Email('test@example.com');
-  var to_email = new helper.Email(sendTo);
+  var to_email = new helper.Email('ysanghavi1@gmail.com');
   var subject = 'Hello World from the SendGrid Node.js Library!';
-  var content = new helper.Content('text/plain', message);
+  var content = new helper.Content('text/plain', 'Hello, Email!');
   var mail = new helper.Mail(from_email, subject, to_email, content);
 
   var request = sg.emptyRequest({
