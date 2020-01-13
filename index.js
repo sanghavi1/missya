e058f0eab4afd63f0b2b39e7b8b0ff2acdce4bf4
Yash Sanghavi <ysanghavi1@gmail.com>
2020-01-12 16:49:28 -0800

    Changed back to test keys after confirming it works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const sendMessageToFirebase = require('./server/sendMessageToFirebase.js');
-const stripe = require('stripe')(process.env.STRIPE_LIVE_KEY);
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 
